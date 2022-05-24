d3787335a8e2f6bca1977db8d06d43ebcc1e5dd1
Roselyn <roselynh100@gmail.com>
2022-05-24 13:26:02 -0400

    Changed favicon. Created routes for project pages.

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 
+import Bandstory from '../views/Bandstory.vue'
+import Listry from '../views/Bandstory.vue'
+import Moonboundconnection from '../views/Bandstory.vue'
+import StoryGenerator from '../views/Bandstory.vue'
+
+
 const routes = [
     {
         path: '/',
@@ -13,6 +19,26 @@ const routes = [
         path: '/about',
         name: 'About',
         component: About
+    },
+    {
+        path: '/bandstory',
+        name: 'Bandstory',
+        component: Bandstory
+    },
+    {
+        path: '/listry',
+        name: 'Listry',
+        component: Listry
+    },
+    {
+        path: '/moonboundconnection',
+        name: 'Moonboundconnection',
+        component: Moonboundconnection
+    },
+    {
+        path: '/storygenerator',
+        name: 'Story Generator',
+        component: StoryGenerator
     }
 ]
 
---
+++ src/views/Bandstory.vue
@@ -0,0 +1,5 @@
+<template>
+
+    <p>testing bandstory uwu</p>
+
+</template>

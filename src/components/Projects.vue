79af83c8cdb1309f322d99eac90774147cde570b
Roselyn <roselynh100@gmail.com>
2022-09-04 00:10:50 -0400

    Correcting Health Screener's tagline

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -20,7 +20,7 @@ import moonboundconnection from '../assets/moonboundconnection.png'
 const projects = [
     {name: "Bandstory", tagline: "Documenting my high school music career", time: "SEPTEMBER 2021", image: bandstory, demo: "https://bandstory.netlify.app/", code: "https://github.com/roselynh100/bandstory"},
     {name: "Listry", tagline: "Saving customers time while grocery shopping", time: "MARCH - MAY 2022", extra: "WINNER OF TECH UNDER 20 CUP '22", image: listry, demo: "", code: "https://github.com/roselynh100/listry"},
-    {name: "Health Screener", tagline: "Creating ideas to combat writer's blocks", time: "JULY 2022", extra: "WINNER OF WEC '22", image: healthscreener, demo: "https://wec-programming-spring-2022.web.app/", code: "https://github.com/roselynh100/health-screener"},
+    {name: "Health Screener", tagline: "Helping hospitals diagnose patients", time: "JULY 2022", extra: "WINNER OF WEC '22", image: healthscreener, demo: "https://wec-programming-spring-2022.web.app/", code: "https://github.com/roselynh100/health-screener"},
     {name: "Moon-Bound Connection", tagline: "Implementing interactivity to songs that have meaning to me", time: "JULY 2021", image: moonboundconnection, demo: "https://moon-bound-connection.netlify.app/", code: "https://github.com/roselynh100/moon-bound-connection"},
     // {name: "", tagline: "", time: "", image: ""}
 

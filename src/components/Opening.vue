7abe690685db5f25be09c9760dc58ebaef538c2f
Roselyn <roselynh100@gmail.com>
2022-12-12 02:28:18 -0500

    Changing opening header

--- src/components/Opening.vue
+++ src/components/Opening.vue
@@ -4,7 +4,7 @@
 
         <p class="text-4xl font-semibold mb-5">Hi! The name's</p>
         <p class="text-7xl md:text-8xl lg:text-9xl font-bold leading-20 text-transparent bg-clip-text bg-gradient-to-r from-indigo-300 to-fuchsia-700 h-40 mb-2">Roselyn Huynh.</p>
-        <p class="text-xl md:text-2xl font-semibold mb-3">I'm a curious front-end developer studying Engineering @ uWaterloo.</p>
+        <p class="text-xl md:text-2xl font-semibold mb-3">I'm a curious software developer studying Engineering @ uWaterloo.</p>
         <p class="text-xl md:text-2xl font-semibold">Currently seeking <span class="italic animate-pulse hover:underline hover:decoration-wavy hover:decoration-indigo-300 hover:animate-none">Summer 2023 internships!</span></p>
 
     </div>
